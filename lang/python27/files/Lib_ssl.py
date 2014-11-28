6fa9a118d019da54740e308811286e597198a5ba
Barnerd <Sp1l@users.noreply.github.com>
2014-11-28 09:38:50 +0100

    Make RAND_egd support conditional

--- lang/python27/files/Lib_ssl.py
+++ lang/python27/files/Lib_ssl.py
@@ -1,12 +1,14 @@
-$OpenBSD: patch-Lib_ssl_py,v 1.1 2014/04/19 13:53:15 espie Exp $
---- Lib/ssl.py.orig	Sat Apr 19 15:49:44 2014
-+++ Lib/ssl.py	Sat Apr 19 15:49:58 2014
-@@ -62,7 +62,7 @@ import _ssl             # if we can't import it, let t
+--- Lib/ssl.py.orig	2014-06-30 04:05:31.000000000 +0200
++++ Lib/ssl.py	2014-11-27 22:49:55.973755982 +0100
+@@ -62,7 +62,10 @@
  from _ssl import OPENSSL_VERSION_NUMBER, OPENSSL_VERSION_INFO, OPENSSL_VERSION
  from _ssl import SSLError
  from _ssl import CERT_NONE, CERT_OPTIONAL, CERT_REQUIRED
 -from _ssl import RAND_status, RAND_egd, RAND_add
-+from _ssl import RAND_status, RAND_add
++try:
++     from _ssl import RAND_status, RAND_egd, RAND_add
++except ImportError:
++     from _ssl import RAND_status, RAND_add
  from _ssl import \
       SSL_ERROR_ZERO_RETURN, \
       SSL_ERROR_WANT_READ, \
