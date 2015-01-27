13e266d1717166e573fb889c59a55f2872f12590
Bernard Spil <spil.oss@gmail.com>
2015-01-27 21:29:50 +0100

    Patches needed revising, can't wait for 2.7.10!

--- lang/python27/files/patch-Lib_ssl.py
+++ lang/python27/files/patch-Lib_ssl.py
@@ -1,14 +1,13 @@
---- Lib/ssl.py.orig	2014-06-30 04:05:31.000000000 +0200
-+++ Lib/ssl.py	2014-11-27 22:49:55.973755982 +0100
-@@ -62,7 +62,10 @@
- from _ssl import OPENSSL_VERSION_NUMBER, OPENSSL_VERSION_INFO, OPENSSL_VERSION
- from _ssl import SSLError
- from _ssl import CERT_NONE, CERT_OPTIONAL, CERT_REQUIRED
--from _ssl import RAND_status, RAND_egd, RAND_add
+--- Lib/ssl.py.orig	2014-12-10 16:59:40.000000000 +0100
++++ Lib/ssl.py	2015-01-27 21:22:33.606365700 +0100
+@@ -107,6 +107,10 @@
+     VERIFY_X509_STRICT)
+ from _ssl import txt2obj as _txt2obj, nid2obj as _nid2obj
+ from _ssl import RAND_status, RAND_egd, RAND_add
 +try:
 +     from _ssl import RAND_status, RAND_egd, RAND_add
 +except ImportError:
 +     from _ssl import RAND_status, RAND_add
- from _ssl import \
-      SSL_ERROR_ZERO_RETURN, \
-      SSL_ERROR_WANT_READ, \
+ 
+ def _import_symbols(prefix):
+     for n in dir(_ssl):
--- lang/python27/files/patch-Modules__ssl.c
+++ lang/python27/files/patch-Modules__ssl.c
@@ -1,6 +1,6 @@
---- Modules/_ssl.c.orig	2014-06-30 04:05:42.000000000 +0200
-+++ Modules/_ssl.c	2014-11-21 11:36:17.902267822 +0100
-@@ -1601,6 +1601,7 @@
+--- Modules/_ssl.c.orig	2014-12-10 16:59:53.000000000 +0100
++++ Modules/_ssl.c	2015-01-27 21:24:47.877731754 +0100
+@@ -3301,6 +3301,7 @@
  It is necessary to seed the PRNG with RAND_add() on some platforms before\n\
  using the ssl() function.");
  
@@ -8,7 +8,7 @@
  static PyObject *
  PySSL_RAND_egd(PyObject *self, PyObject *arg)
  {
-@@ -1626,6 +1627,7 @@
+@@ -3326,6 +3327,7 @@
  Queries the entropy gather daemon (EGD) on the socket named by 'path'.\n\
  Returns number of bytes read.  Raises SSLError if connection to EGD\n\
  fails or if it does not provide enough data to seed PRNG.");
@@ -16,12 +16,12 @@
  
  #endif /* HAVE_OPENSSL_RAND */
  
-@@ -1640,8 +1642,10 @@
+@@ -3720,8 +3722,10 @@
  #ifdef HAVE_OPENSSL_RAND
      {"RAND_add",            PySSL_RAND_add, METH_VARARGS,
       PySSL_RAND_add_doc},
-+#ifdef HAVE_RAND_EGD 
-     {"RAND_egd",            PySSL_RAND_egd, METH_O,
++#ifdef HAVE_RAND_EGD
+     {"RAND_egd",            PySSL_RAND_egd, METH_VARARGS,
       PySSL_RAND_egd_doc},
 +#endif /* HAVE_RAND_EGD */
      {"RAND_status",         (PyCFunction)PySSL_RAND_status, METH_NOARGS,
