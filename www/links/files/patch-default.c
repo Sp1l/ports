b935e243f82b1bce24bb8e141d54092bc5d64b2a
Bernard Spil <spil.oss@gmail.com>
2015-03-21 13:33:14 +0100

    Proper fix www/links LibreSSL build

--- www/links/files/patch-default.c
+++ www/links/files/patch-default.c
@@ -1,7 +1,7 @@
---- default.c.orig	2008-03-06 22:43:25.117856165 -0500
-+++ default.c	2008-03-06 22:44:12.412560939 -0500
-@@ -400,7 +400,7 @@
- 	get_system_name();
+--- default.c.orig	2013-08-25 23:08:07 UTC
++++ default.c
+@@ -809,7 +809,7 @@ void init_home(void)
+ 	get_compiler_name();
  	links_home = get_home(&first_use);
  	if (!links_home) {
 -		fprintf(stderr, "Unable to find or create links config directory. Please check, that you have $HOME variable set correctly and that you have write permission to your home directory.\n\007");
--- www/links/files/patch-https.c
+++ www/links/files/patch-https.c
@@ -1,11 +1,14 @@
---- https.c.orig	2013-09-20 23:17:00.000000000 +0200
-+++ https.c	2015-02-27 12:35:41.718975799 +0100
-@@ -36,7 +36,7 @@
+--- https.c.orig	2013-09-20 21:17:00 UTC
++++ https.c
+@@ -36,7 +36,10 @@ SSL *getSSL(void)
  		unsigned os_pool_size;
  
  		const unsigned char *f = (const unsigned char *)RAND_file_name(cast_char f_randfile, sizeof(f_randfile));
 -		if (f && RAND_egd(cast_const_char f) < 0) {
-+		if (f) {
++#ifdef HAVE_RAND_EGD
++		if (f && RAND_egd(cast_const_char f) < 0) 
++#endif
++		{
  			/* Not an EGD, so read and write to it */
  			if (RAND_load_file(cast_const_char f_randfile, -1))
  				RAND_write_file(cast_const_char f_randfile);
