6d0d9b8b901c066d92e6c6746e8c9fa385ae6009
Sp1l <spil.oss@gmail.com>
2015-03-30 15:08:50 +0200

    Proper fix www/webstone-ssl LibreSSL build

--- www/webstone-ssl/files/patch-get.c
+++ www/webstone-ssl/files/patch-get.c
@@ -1,12 +1,27 @@
---- get.c.orig	2000-06-21 21:15:41.000000000 +0200
-+++ get.c	2015-03-07 23:55:19.316239812 +0100
-@@ -176,20 +176,26 @@ get(char *loc, NETPORT port, char *url, 
- 			}
- 			D_PRINTF( "SSL version = SSLv23\n" );
- 		}
+--- get.c.orig	2000-06-21 19:15:41 UTC
++++ get.c
+@@ -55,6 +55,8 @@
+ #ifdef STONE_SSL
+ #include <openssl/ssl.h>
+ #include <openssl/bio.h>
++#include <openssl/opensslconf.h>
++#include <openssl/err.h>
+ #endif
+ 
+ #include "sysdep.h"
+@@ -169,27 +171,33 @@ get(char *loc, NETPORT port, char *url, 
+       OpenSSL_add_all_algorithms();
+       OpenSSL_add_ssl_algorithms();
+ 
+-		if(ssl_version != NULL && strcasecmp(ssl_version, "SSLv23") == 0) {
+-			if((ctx = SSL_CTX_new(SSLv23_client_method())) == NULL) {
+-				ERR_print_errors_fp(stderr);
+-				goto error;
+-			}
+-			D_PRINTF( "SSL version = SSLv23\n" );
+-		}
 -		else if(ssl_version != NULL && strcasecmp(ssl_version, "SSLv2") == 0) {
-+		else
-+#ifdef OPENSSL_NO_SSL2
++#ifndef OPENSSL_NO_SSL2
 +		if(ssl_version != NULL && strcasecmp(ssl_version, "SSLv2") == 0) {
  			if((ctx = SSL_CTX_new(SSLv2_client_method())) == NULL) {
  				ERR_print_errors_fp(stderr);
@@ -17,15 +32,23 @@
 -		else {
 +		else
 +#endif
-+#ifndef OPENSSL_NO_SSL3
-+		{
++#ifndef OPENSSL_NO_SSL3_METHOD
++		if(ssl_version != NULL && strcasecmp(ssl_version, "SSLv3") == 0) {
  			if((ctx = SSL_CTX_new(SSLv3_client_method())) == NULL) {
  				ERR_print_errors_fp(stderr);
  				goto error;
  			}
  			D_PRINTF( "SSL version = SSLv3\n" );
  		}
++		else
 +#endif
++		{
++			if((ctx = SSL_CTX_new(SSLv23_client_method())) == NULL) {
++				ERR_print_errors_fp(stderr);
++				goto error;
++			}
++			D_PRINTF( "SSL version = SSLv23\n" );
++		}
  		
  		/* set preferred cipher */
  		if(ssl_cipher != NULL && strlen(ssl_cipher) > 0) {
