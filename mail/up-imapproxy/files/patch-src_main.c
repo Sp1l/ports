1ee92f085681713bdc4e60ce56f3c06cc35093db
Sp1l <spil.oss@gmail.com>
2015-05-16 12:00:07 +0200

    Proper fix mail/up-imapproxy LibreSSL build

--- mail/up-imapproxy/files/patch-src_main.c
+++ mail/up-imapproxy/files/patch-src_main.c
@@ -1,13 +1,12 @@
---- src/main.c.orig	2010-07-26 09:21:19.000000000 +0200
-+++ src/main.c	2015-02-23 14:54:36.605219022 +0100
-@@ -471,8 +471,8 @@
- 	    /* Set up OpenSSL thread protection */
+--- src/main.c.orig	2010-07-26 07:21:19 UTC
++++ src/main.c
+@@ -472,7 +472,9 @@ int main( int argc, char *argv[] )
  	    ssl_thread_setup(fn);
  	    
--            /* Need to seed PRNG, too! */
--            if ( RAND_egd( ( RAND_file_name( f_randfile, sizeof( f_randfile ) ) == f_randfile ) ? f_randfile : "/.rnd" ) ) 
-+            /* Need to seed PRNG, too! 
-+            if ( RAND_egd( ( RAND_file_name( f_randfile, sizeof( f_randfile ) ) == f_randfile ) ? f_randfile : "/.rnd" ) ) */
+             /* Need to seed PRNG, too! */
++#ifdef HAVE_RAND_EGD
+             if ( RAND_egd( ( RAND_file_name( f_randfile, sizeof( f_randfile ) ) == f_randfile ) ? f_randfile : "/.rnd" ) ) 
++#endif
  	    {
                  /* Not an EGD, so read and write it. */
                  if ( RAND_load_file( f_randfile, -1 ) )
