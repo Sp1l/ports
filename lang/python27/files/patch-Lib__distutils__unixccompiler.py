054652b102c51f04ef430032060a3574ef204fa4
Bernard Spil <spil.oss@gmail.com>
2015-05-24 11:06:32 +0200

    Update lang/python27 to 2.7.10

--- lang/python27/files/patch-Lib__distutils__unixccompiler.py
+++ lang/python27/files/patch-Lib__distutils__unixccompiler.py
@@ -2,9 +2,9 @@
 # Issue ID: http://bugs.python.org/issue20767
 # Submitted by: antoine
 
---- Lib/distutils/unixccompiler.py.orig	2014-12-10 15:59:34 UTC
-+++ Lib/distutils/unixccompiler.py
-@@ -228,6 +228,8 @@ class UnixCCompiler(CCompiler):
+--- ./Lib/distutils/unixccompiler.py.orig      2013-11-10 07:36:40.000000000 +0000
++++ ./Lib/distutils/unixccompiler.py   2014-02-19 15:41:48.000000000 +0000
+@@ -228,6 +228,8 @@
          if sys.platform[:6] == "darwin":
              # MacOSX's linker doesn't understand the -R flag at all
              return "-L" + dir
