85a1d4306b667bd18405f50b42eee70d8bb2d6b4
Bernard Spil <spil.oss@gmail.com>
2016-01-27 16:15:21 +0100

    Add upstreaming of the getdirect patch

--- ftp/curl/files/patch-docs_examples_getredirect.c
+++ ftp/curl/files/patch-docs_examples_getredirect.c
@@ -1,3 +1,5 @@
+Committed upstream https://github.com/bagder/curl/commit/e400a89b18
+
 --- docs/examples/getredirect.c.orig	2016-01-14 15:37:49 UTC
 +++ docs/examples/getredirect.c
 @@ -48,7 +48,7 @@ int main(void)
