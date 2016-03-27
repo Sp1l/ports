581f7d6577f9853b6d18cfbb0867ef9688aa612c
Bernard Spil <spil.oss@gmail.com>
2016-03-27 12:43:33 +0200

    databases/mariadb101-{server,client}: Update to 10.1.13
      - Update to version 10.1.13
      - Make GSSAPI plugin optional [2]
      - Remove patches that are merged upstream
      - Server
        - Fix OQGraph build failure [1]
        - Install server gssapi plugin only
        - Add license and readme file for TokuDB
        - Add tokuft_logprint and tokuftdump binaries
      - Client
        - Install client gssapi plugin
        - Enable PLIST_SUB for optional gssapi plugin
        - Add new plugins and ini file
    
    PR:		207675 [1], 207883 [2]
    Reviewed_by:	feld (mentor), koobs (mentor)
    Approved bu:	(mentor)
    Differential_Revision:	D5751

--- databases/mariadb101-server/files/patch-storage_tokudb_PerconaFT_cmake__modules_TokuThirdParty.cmake
+++ databases/mariadb101-server/files/patch-storage_tokudb_PerconaFT_cmake__modules_TokuThirdParty.cmake
@@ -1,7 +1,7 @@
-Replace bundled liblzma library with base lzma
+Do NOT use bundled libs, use xz/lzma from base and archivers/snappy from ports
 
---- storage/tokudb/PerconaFT/cmake_modules/TokuThirdParty.cmake.orig	2015-12-23 16:33:32.000000000 +0100
-+++ storage/tokudb/PerconaFT/cmake_modules/TokuThirdParty.cmake	2016-01-03 17:23:42.102122634 +0100
+--- storage/tokudb/PerconaFT/cmake_modules/TokuThirdParty.cmake.orig	2016-01-28 12:12:55 UTC
++++ storage/tokudb/PerconaFT/cmake_modules/TokuThirdParty.cmake
 @@ -34,6 +34,13 @@ if (CMAKE_PROJECT_NAME STREQUAL TokuDB)
      endif ()
  endif ()
@@ -16,12 +16,24 @@ Replace bundled liblzma library with base lzma
  ## add lzma with an external project
  set(xz_configure_opts --with-pic --enable-static)
  if (APPLE)
-@@ -105,7 +112,7 @@ add_library(lzma STATIC IMPORTED)
+@@ -105,8 +112,15 @@ add_library(lzma STATIC IMPORTED)
  set_target_properties(lzma PROPERTIES IMPORTED_LOCATION
    "${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_CFG_INTDIR}/xz/lib/liblzma.a")
  add_dependencies(lzma build_lzma)
--
 +ENDIF()
  
++IF (CMAKE_SYSTEM_NAME STREQUAL "FreeBSD")
++  include_directories("/usr/local/include")
+ 
++  add_library(snappy SHARED IMPORTED)
++  set_target_properties(snappy PROPERTIES IMPORTED_LOCATION
++    "/usr/local/lib/libsnappy.so")
++ELSE()
  ## add snappy with an external project
  set(SNAPPY_SOURCE_DIR "${TokuDB_SOURCE_DIR}/third_party/snappy-1.1.2" CACHE FILEPATH "Where to find sources for snappy.")
+ if (NOT EXISTS "${SNAPPY_SOURCE_DIR}/CMakeLists.txt")
+@@ -140,3 +154,4 @@ add_library(snappy STATIC IMPORTED)
+ set_target_properties(snappy PROPERTIES IMPORTED_LOCATION
+   "${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_CFG_INTDIR}/snappy/lib/libsnappy.a")
+ add_dependencies(snappy build_snappy)
++ENDIF()
