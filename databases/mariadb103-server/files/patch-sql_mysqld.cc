426484c443fd1734eae5aa063b0768c11edd55b6
Bernard Spil <brnrd@FreeBSD.org>
2018-06-04 17:04:18 +0200

    Update MariaDB 10.3 to latest version
     - Works but still has conflicts between server and client

--- databases/mariadb103-server/files/patch-sql_mysqld.cc
+++ databases/mariadb103-server/files/patch-sql_mysqld.cc
@@ -1,13 +1,5 @@
 --- sql/mysqld.cc.orig	2017-05-14 23:13:18 UTC
 +++ sql/mysqld.cc
-@@ -111,6 +111,7 @@
- #endif
- 
- #include <my_systemd.h>
-+#include <my_crypt.h>
- 
- #define mysqld_charset &my_charset_latin1
- 
 @@ -120,6 +121,7 @@
  #define HAVE_CLOSE_SERVER_SOCK 1
  #endif
