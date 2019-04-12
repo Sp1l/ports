d24e19d184092b0bcae81e2aea750fef3ba0df2b
Bernard Spil <brnrd@FreeBSD.org>
2019-04-12 08:30:09 +0200

    Update to latest from FreeBSD ports

--- www/nextcloud/files/patch-config_config.documented.php
+++ www/nextcloud/files/patch-config_config.documented.php
@@ -1,12 +1,12 @@
---- config/config.sample.php.orig	2018-03-13 18:47:31 UTC
-+++ config/config.sample.php
+--- config/config.documented.php.orig	2018-03-13 18:47:31 UTC
++++ config/config.documented.php
 @@ -818,6 +818,11 @@ $CONFIG = array(
  		'url' => '/apps',
  		'writable' => true,
  	),
 +	array(
 +		'path'=> '/var/www/nextcloud/apps-pkg',
-+		'url' => '/var/www/apps-pkg',
++		'url' => '/apps-pkg',
 +		'writable' => false,
 +	),
  ),
